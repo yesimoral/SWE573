b734acdf97e3077817e9577f79477f0bf00381ef
yesimoral <yesimoral89@gmail.com>
2017-04-30 23:27:00 +0300

    Home and Followers page completed

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,6 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'TwitterController'
   });
-}])
-
-.controller('View2Ctrl', [function() {
-
 }]);
