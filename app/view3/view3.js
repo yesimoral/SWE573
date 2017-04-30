6b37e208a9d28a601119d04fd59ac42b354e49b1
yesimoral <yesimoral89@gmail.com>
2017-05-01 01:53:41 +0300

    map

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -5,10 +5,6 @@ angular.module('myApp.view3', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view3', {
     templateUrl: 'view3/view3.html',
-    controller: 'View3Ctrl'
+    controller: 'MapCtrl'
   });
-}])
-
-.controller('View3Ctrl', [function() {
-
 }]);
